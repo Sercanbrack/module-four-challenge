fc2c22ba77b793e09d2a8929c876034597622cd2
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-05-16 18:34:17 -0600

    Reworked logic AGAIN, should run much better now

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,29 @@ var startButton = document.getElementById('start')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var timerElement = document.getElementById('timer')
-var buttonZeroElement = document.getElementById('button-0')
-var buttonOneElement = document.getElementById('button-1')
-var buttonTwoElement = document.getElementById('button-2')
-var buttonThreeElement = document.getElementById('button-3')
 var instructionsElement = document.getElementById('instructions')
 let timer = 75
 startButton.addEventListener('click', startQuiz)
 
+const questions = [
+    {
+        "text": "Javascript is a ________ programming language.",
+        "answers": {
+            "correct": ["Loosely typed"],
+            "incorrect": ["Strongly typed", "Static", "Server-side"]
+        }
+    }
+]
+
+function answerLogic(buttonElement, question) {
+    if (question.answers.correct.includes(buttonElement.innerText)) {
+        // Perform correct actions
+        correctAnswer()
+    } else {
+        // Vice-versa
+        incorrectAnswer()
+    }
+}
 
 function startQuiz() {
     console.log('started!')
@@ -19,7 +34,26 @@ function startQuiz() {
 
     instructionsElement.classList.add('hide')
     timerElement.append(timer)
-    question1()
+
+    questions.forEach(question => {
+        questionElement.innerText = question.text
+
+        const buttonsDiv = document.getElementById("answer-buttons")
+        buttonsDiv.innerHTML = "" // Clear the div
+        let mergedAnswers = question.answers.incorrect
+        mergedAnswers = mergedAnswers.concat(question.answers.correct)
+        console.log(JSON.stringify(question.answers, null, " "))
+        mergedAnswers = shuffle(mergedAnswers)
+        
+        // Now we create the buttons
+        mergedAnswers.forEach(answer => {
+            const button = document.createElement("button")
+            button.innerText = answer
+            button.addEventListener('click', () => {answerLogic(button, question)})
+            buttonsDiv.appendChild(button)
+        })
+    })
+    // question1()
 }
 function correctAnswer() {
     questionElement.innerText = "Correct Answer!"
@@ -30,60 +64,20 @@ function incorrectAnswer() {
     questionElement.innerText = "Incorrect Answer. -5 seconds."
     questionElement.style.color = "var(--incorrect)"
 }
-function question1() {
-    let question = questions[0]
-    let answer0 = answers0[0]
-    let answer1 = answers0[1]
-    let answer2 = answers0[2]
-    let answer3 = answers0[3]
-    questionElement.innerText = question
-    buttonZeroElement.innerText = answer0
-    buttonOneElement.innerText = answer1
-    buttonTwoElement.innerText = answer2
-    buttonThreeElement.innerText = answer3
-    buttonZeroElement.addEventListener('click', incorrectAnswer())
-    buttonOneElement.addEventListener('click', incorrectAnswer())
-    buttonTwoElement.addEventListener('click', correctAnswer())
-    buttonThreeElement.addEventListener('click', incorrectAnswer())
+
+
+/**
+ * Shuffles array in place using the Fisher-Yates shuffle algorithm.
+ * @param {questions} a items An array containing the items.
+ */
+ function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    console.log(a)
+    return a;
 }
-
-function question2() {
-
-}
-
-function question3() {
-
-}
-
-function question4() {
-
-}
-
-function question5() {
-
-}
-
-var questions = [
-    "Javascript is a ______ programming language.",
-    "What does the 'var' statement do in Javascript?",
-    "What file extension do Javascript files save as?" ,
-    "What do these symbols do in Javascript?: '[]'",
-    "What is a true/false statement referred to as in Javascript?"
-]
-
-var answers0 = [
-    "Strongly typed", "Static", "Loosely typed", "Server-side"
-]
-
-var answers1 = [
-    "Creates an array", "Creates an object", "Creates a function", "Creates a variable"
-]
-var answers2 = [
-    ".css", ".js", ".java", ".docx"
-]
-var answers3 = [
-    "Create an array", "Create an object", "Create a function", "Create a break point"
-]
-var answers4 = [
-    "Boolean", "If/Then", "If/Else", "DOM"
-]
