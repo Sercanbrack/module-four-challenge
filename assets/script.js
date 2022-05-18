dc279e702cce47e0ae028ec169c2e528a758a2d2
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-05-18 11:53:14 -0600

    Updated logic again

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var timerElement = document.getElementById('timer')
 var instructionsElement = document.getElementById('instructions')
-let timer = 75
+let timer = 30
 startButton.addEventListener('click', startQuiz)
 
 const questions = [
@@ -13,17 +13,48 @@ const questions = [
             "correct": ["Loosely typed"],
             "incorrect": ["Strongly typed", "Static", "Server-side"]
         }
+    },
+    {
+        "text": "What do the following symbols create?: '[ ]'",
+        "answers": {
+            "correct": ["An array"],
+            "incorrect": ["A function", "A variable", "A string"]
+        }
+    },
+    {
+        "text": "What does the 'var' statement do?",
+        "answers": {
+            "correct": ["Creates a variable"],
+            "incorrect": ["Creates an array", "Creates a function", "Concatenates two arrays"]
+        }
     }
 ]
 
+let answerCorrect = false
 function answerLogic(buttonElement, question) {
     if (question.answers.correct.includes(buttonElement.innerText)) {
-        // Perform correct actions
         correctAnswer()
     } else {
-        // Vice-versa
         incorrectAnswer()
     }
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.text
+    questionElement.style.color = "black"
+    const buttonsDiv = document.getElementById("answer-buttons")
+    buttonsDiv.innerHTML = "" 
+    let mergedAnswers = question.answers.incorrect
+    mergedAnswers = mergedAnswers.concat(question.answers.correct)
+    console.log(JSON.stringify(question.answers, null, " "))
+    mergedAnswers = shuffle(mergedAnswers)
+    
+    mergedAnswers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer
+        button.addEventListener('click', () => {answerLogic(button, question)})
+        buttonsDiv.appendChild(button)
+    })
 }
 
 function startQuiz() {
@@ -31,45 +62,27 @@ function startQuiz() {
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
     timerElement.classList.remove('hide')
-
-    instructionsElement.classList.add('hide')
     timerElement.append(timer)
-
+    instructionsElement.classList.add('hide')
+    
     questions.forEach(question => {
-        questionElement.innerText = question.text
+        showQuestion(question)
 
-        const buttonsDiv = document.getElementById("answer-buttons")
-        buttonsDiv.innerHTML = "" // Clear the div
-        let mergedAnswers = question.answers.incorrect
-        mergedAnswers = mergedAnswers.concat(question.answers.correct)
-        console.log(JSON.stringify(question.answers, null, " "))
-        mergedAnswers = shuffle(mergedAnswers)
         
-        // Now we create the buttons
-        mergedAnswers.forEach(answer => {
-            const button = document.createElement("button")
-            button.innerText = answer
-            button.addEventListener('click', () => {answerLogic(button, question)})
-            buttonsDiv.appendChild(button)
-        })
     })
-    // question1()
 }
+
 function correctAnswer() {
     questionElement.innerText = "Correct Answer!"
     questionElement.style.color = "var(--correct)"
-    setTimeout(() => {console.log("Correct answer!"); }, 2000)
+    questionElement.innerText = "Correct Answer!"
+    answerCorrect = true
 }
 function incorrectAnswer() {
     questionElement.innerText = "Incorrect Answer. -5 seconds."
     questionElement.style.color = "var(--incorrect)"
 }
 
-
-/**
- * Shuffles array in place using the Fisher-Yates shuffle algorithm.
- * @param {questions} a items An array containing the items.
- */
  function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
